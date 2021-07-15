c14913435c9a2298918c96e3cdecdec2741494e8
DevVatu <devvatu@gmail.com>
2021-07-15 12:12:34 +0200

    added marilynart

--- projectsObject.js
+++ projectsObject.js
@@ -4,7 +4,7 @@ export const projects = [
     name: "Headset promotion",
     link: "https://szathrobi.github.io/Headset-Promotion/",
     codeLink: "https://github.com/SzathRobi/Headset-Promotion/tree/master",
-    desc: "The purpose of this template is that I wanted to showcase a single product with and appealing design. Wireframing in Figma is fast and easy so I could design this in time. After that I used React for this SPA and hosted on github pages to make it visible for everyone.",
+    desc: "The purpose of this template is to showcase a single product with and appealing design. Wireframing in Figma is fast and easy for me so I could design this in no time. After that I used React for this SPA and hosted on github pages to make it visible for everyone.",
     images: [
       "/project_imgs/headset_1",
       "/project_imgs/headset_2",
@@ -17,7 +17,7 @@ export const projects = [
     name: "Raven hotel",
     link: "https://szathrobi.github.io/raven-hotel/",
     codeLink: "https://github.com/SzathRobi/raven-hotel/tree/master",
-    desc: "Clean and responsive hotel website template with many pages like Home, Blog, About etc. . Here I utilized Material-UI to help me get a very clean and user friendly design. Feel free to get some ihlet from the design!",
+    desc: "Clean and responsive hotel website template with many pages like Home, Blog, About etc. Here I utilized Material-UI to help me get a very clean and user friendly design. It was a few days to make it and I enjoyed the process very much!",
     images: [
       "/project_imgs/raven_1",
       "/project_imgs/raven_2",
@@ -30,7 +30,7 @@ export const projects = [
     name: "Stabofficial",
     link: "https://modest-hermann-4b68b8.netlify.app/#main",
     codeLink: "https://github.com/SzathRobi/stabofficial",
-    desc: "This site was made for client who had a video clip and advertisement filming group. My job was to redesign they old site, and they were very happy about the result. Unfortunately, this is an unfinished project due to the covid. The group had to split up. I think you can still check their old website at www.stabofficial.hu. Compare with my version!",
+    desc: "This site was made for client who had a video clip and advertisement filming group. My job was to redesign their old site, and they were very amazed with the result. Unfortunately, this is an unfinished project due to the group had to split up because of the covid. I think you can still check their old website at www.stabofficial.hu. Compare with my version! Hope you will see the difference.",
     images: [
       "/project_imgs/stab_1",
       "/project_imgs/stab_2",
@@ -43,12 +43,25 @@ export const projects = [
     name: "BudgetGrow",
     link: "https://budgetgrow.vercel.app/",
     codeLink: "https://github.com/SzathRobi/BudgetGrow",
-    desc: "I made this budget tracker app to help me organize my money a bit more. It started out as a React Native project but since I dont have a google developer account, I decided to make a PWA with Next.js on the frontend deployed to Vercel, Strapi connected to Mongodb as my backend and Headles CMS deployed on Heroku with Kaffeine. ",
+    desc: "I made this budget tracker app to help me organize my money a bit more. I tried to find something on the appstore but most of them was too compicated for my problem. It started out as a React Native project but since I dont have a google developer account, I couldn't put it on the appstore. I decided to make a PWA with Next.js on the frontend deployed to Vercel, Strapi connected to Mongodb as my backend and Headles CMS deployed on Heroku with Kaffeine. ",
     images: [
       "/project_imgs/budget_1",
       "/project_imgs/budget_2",
       "/project_imgs/budget_3",
     ],
     stack: ["html", "css", "js", "next", "strapi", "mongodb"],
+  },
+  {
+    id: 5,
+    name: "MarilynArt",
+    link: "https://marilynart.vercel.app/",
+    codeLink: "https://github.com/SzathRobi/marilynArtFrontend",
+    desc: 'I was asked to make an Ecommerce site for a client who had alredy 2 phisical shop. I setted up strapi as the headles CMS of the app, utilized Next.js for the frontend to make it super perfomant, and used the React Context API for some state management. I used Material-UI to provide a good UI and UX and bring out the maximum from the site. It was one of free "step in the field" project for me. Unfortunately, the client was not so motivated as me, and when we were near to finish the site, as soon he had to put a little work in it he shut down the whole project',
+    images: [
+      "/project_imgs/marilyn_1",
+      "/project_imgs/marilyn_2",
+      "/project_imgs/marilyn_3",
+    ],
+    stack: ["html", "css", "js", "next", "strapi", "material-ui"],
   },
 ];
